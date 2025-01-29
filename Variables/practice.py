dbe0e18fe3557d1233c2e4ab4736d326c5aa28dc
Jameson-B <jameson.bultez@ucas-edu.net>
2025-01-29 11:49:49 -0700

    Added print statements to practice.py.

--- Variables/practice.py
+++ Variables/practice.py
@@ -2,19 +2,28 @@ name = "Jameson"
 print(name)
 
 smallNumber = 6
+print(smallNumber)
 
 largeNumber = 201
+print(largeNumber)
 
 breakfast = "banana"
+print(breakfast)
 
 favColor = "blue"
+print(favColor)
 
 school = "UCAS"
+print(school)
 
 currentYear = 2025
+print(currentYear)
 
 eyeColor = "brown"
+print(eyeColor)
 
 age = 16
+print(age)
 
 favSubject = "geography"
+print(favSubject)
