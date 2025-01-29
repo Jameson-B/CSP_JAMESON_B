194d7c2a0944a4ae6cfae1b0e05bac995545e26d
Jameson-B <jameson.bultez@ucas-edu.net>
2025-01-29 18:27:21 +0000

    Wrote ten variables about myself.

---
+++ Variables/practice.py
@@ -0,0 +1,19 @@
+name = "Jameson"
+
+smallNumber = 6
+
+largeNumber = 42
+
+breakfast = "banana"
+
+favColor = "blue"
+
+school = "UCAS"
+
+currentYear = 2025
+
+eyeColor = "brown"
+
+age = 16
+
+favSubject = "geography"
