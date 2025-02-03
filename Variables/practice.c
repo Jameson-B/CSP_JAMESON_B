55774f42330bc865a298031bf7ee3a5afab7fdf7
Jameson-B <jameson.bultez@ucas-edu.net>
2025-02-03 12:38:46 -0700

    Update practice.c

--- Variables/practice.c
+++ Variables/practice.c
@@ -1,7 +1,27 @@
+// Jameson Bultez, Variables Practice C
+
+#include <stdio.h>
+
+main()
+
 char name[] = "Jameson";
 
-int smallNumber[] = 3;
+int smallNumber = 3;
 
-int largeNumber[] = 130;
+int largeNumber = 130;
 
 char breakfast[] = "banana";
+
+char favColor[] = "blue";
+
+char school[] = "UCAS";
+
+int currentYear = 2025;
+
+char eyeColor[] = "brown";
+
+int age = 16;
+
+char favSubject[] = "geography"
+
+
