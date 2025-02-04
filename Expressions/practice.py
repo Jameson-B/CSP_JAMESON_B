6a8a32b42a9f9e71773f0ca35aa27c253d2e8638
Jameson-B <jameson.bultez@ucas-edu.net>
2025-02-04 11:36:02 -0700

    Edited expression practice Python

--- Expressions/practice.py
+++ Expressions/practice.py
@@ -1,7 +1,12 @@
 # Jameson Bultez, Expressions Practice Python
 
-health = 7
-armor = 6
+playerHealth = 7
+playerArmor = 6
 monsterPower = 24
 monsterWeakness = 8
 mapBonus = 4
+
+playerHealth = playerHealth - monsterPower / monsterWeakness * mapBonus + playerArmor
+print(playerHealth)
+
+monsterHealth = monsterHealth / monsterWeakness - 
