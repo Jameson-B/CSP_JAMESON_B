9b6721cbe16e5dae8eead24334185818a3ddd308
Jameson-B <jameson.bultez@ucas-edu.net>
2025-02-04 11:47:03 -0700

    Edited Python expression practice.

--- Expressions/practice.py
+++ Expressions/practice.py
@@ -9,4 +9,12 @@ mapBonus = 4
 playerHealth = playerHealth - monsterPower / monsterWeakness * mapBonus + playerArmor
 print(playerHealth)
 
-monsterHealth = monsterHealth / monsterWeakness - 
+#monsterHealth = monsterHealth / monsterWeakness - 
+
+print(18 / 3 - 7  + 2 * 5)
+
+print(6 * 4 / 12 + 72 / 8 - 9)
+
+print(17 - 6 / 2 + 4 * 3)
+
+print(-2*(1 * 4 - 2 / 2) + 6 + 2 - 3)
