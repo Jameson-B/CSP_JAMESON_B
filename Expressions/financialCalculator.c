6cd36baba5c451ece04eaf7c3c05da39e3f190e7
Jameson-B <jameson.bultez@ucas-edu.net>
2025-02-07 11:50:15 -0700

    Worked on finance calculator C

--- Expressions/financialCalculator.c
+++ Expressions/financialCalculator.c
@@ -13,78 +13,77 @@ float percentRent;
 float percentUtilities;
 float percentGroceries;
 float percentTransport;
-float percentSavings;
 float percentSpending;
 
 int main(void) {
      
      // printf statement that welcomes my user and tells them what my program does.
-/*
-     // ask user what their income is (variable that is an input).
-     printf("What is your monthly income?:");
-     
-     float income = input("What is your monthly income?: \n"));
+     printf("Welcome to the monthly financial calculator, C edition. \n \n");
 
+     // ask user what their income is (variable that is an input).
+     printf("What is your monthly income?: \n");
+     scanf("%f", &income);
+     
      // ask user what their rent/mortgage is (variable that is an input).
-     rent = float(input("How much is your rent/mortgage?: \n"));
+     printf("How much is your rent/mortgage?: \n");
+     scanf("%f", &rent);
 
      // ask user what their utilities cost (variable that is an input).
-     utilities = float(input("How much do your utilities cost?: \n"));
+     printf("How much do your utilities cost?: \n");
+     scanf("%f", &utilities);
 
      // ask user what their grocereris cost (variable that is an input).
-     groceries = float(input("How much do your groceries cost?: \n"));
+     printf("How much do your groceries cost?: \n");
+     scanf("%f", &groceries);
 
      // ask user what their transportation costs (variable that is an input).
-     transport = float(input("How much does your gas/bus fare cost?: \n"));
+     printf("How much does your gas/bus fare cost?: \n");
+     scanf("%f", &transport);
 
      // calculate savings as 10% of income (income * .1). (variable that is a float).
-     savings = round(income*.1, 2);
+     savings = income*.1;
 
      // calculate spending as income - savings - rent/mortgage - utilities - groceries - transportation (variable that is a float)
-     spending = round(income-savings-rent-utilities-groceries-transport, 2);
+     spending = income-savings-rent-utilities-groceries-transport;
 
      // caclulate percent income of rent/mortgage (rent / income * 100) (variable)
-     percentRent = round(rent/income*100, 1);
+     percentRent = rent/income*100;
 
      // caclulate percent income of utilities (utilities / income * 100) (variable)
-     percentUtilities = round(utilities/income*100, 1);
+     percentUtilities = utilities/income*100;
 
      // caclulate percent income of groceries (groceries / income * 100) (variable)
-     percentGroceries = round(groceries/income*100, 1);
+     percentGroceries = groceries/income*100;
 
      // caclulate percent income of transportation (transportation / income * 100) (variable)
-     percentTransport = round(transport/income*100 ,1);
-
-     // calculate percent income of savings (percentsavings / income * 100) (variable)
-     percentSavings = round(savings/income*100, 1);
+     percentTransport = transport/income*100;
 
      // calculte percent income of spending (spending / income * 100) (variable)
-     percentSpending = round(spending/income*100, 1);
+     percentSpending= spending/income*100;
 
-     // white space printf statement
+     // white space print statement
      printf("");
 
      // Your rent is $XX.XX, which is XX% of your income (printf statement).
-     printf("Your rent is $", rent, ", which is", percentRent, "percent of your income.");
+     printf("Your rent is $%f, which is %f percent of your income. \n", rent, percentRent);
 
      // Your utilities is $XX.XX, which is XX% of your income (printf statement).
-     printf("Your utilities cost $", utilities, ", which is", percentUtilities, "percent of your income.");
+     printf("Your utilities cost $%f, which is %f percent of your income. \n", utilities, percentUtilities);
 
      // Your groceries is $XX.XX, which is XX% of your income (printf statement).
-     printf("Your groceries cost $", groceries, ", which is", percentGroceries, "percent of your income.");
+     printf("Your groceries cost $%f, which is %f percent of your income. \n", groceries, percentGroceries);
 
      // Your transportation is $XX.XX, which is XX% of your income (printf statement).
-     printf("Your transportation costs $", transport, ", which is", percentTransport, "percent of your income.");
+     printf("Your gas/bus fare costs $%f, which is %f percent of your income. \n", transport, percentTransport);
 
      // Your savings is $XX.XX, which is XX% of your income (printf statement).
-     printf("Your savings are $", savings, ", which is", percentSavings, "percent of your income.");
+     printf("Your savings are $%f, which is 10 percent of your income. \n", savings);
 
      // Your spending is $XX.XX, which is XX% of your income (printf statement).
-     printf("Your spending is $", spending, ", which is", percentSpending, "percent of your income. \n");
+     printf("Your spending is $%f, which is %f percent of your income. \n", spending, percentSpending);
 
      // printf statement that thanks the user and ends the conversation.
      printf("Thank you for using the monthly financial calculator. Spend wisely!");
-*/
 
      return 0;
 }
