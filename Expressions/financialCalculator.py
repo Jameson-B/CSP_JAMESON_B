921fb123336150ebae787e06a4799e6b189e04a5
Jameson-B <jameson.bultez@ucas-edu.net>
2025-02-07 08:12:55 +0000

    Completed finance calculator python

--- Expressions/financialCalculator.py
+++ Expressions/financialCalculator.py
@@ -1,14 +1,13 @@
 # Jameson Bultez, Financial Calculator Python
 
 # print statement that welcomes my user and tells them what my program does.
-print("Welcome! This is your monthly financial calculator.")
+print("Welcome! This is the monthly financial calculator. \n")
 
 # ask user what their income is (variable that is an input).
 income = float(input("What is your monthly income?: \n"))
 
 # ask user what their rent/mortgage is (variable that is an input).
-rent = float(input("How much does your rent/mortgage cost?: \n"))
-
+rent = float(input("How much is your rent/mortgage?: \n"))
 
 # ask user what their utilities cost (variable that is an input).
 utilities = float(input("How much do your utilities cost?: \n"))
@@ -20,39 +19,50 @@ groceries = float(input("How much do your groceries cost?: \n"))
 transport = float(input("How much does your gas/bus fare cost?: \n"))
 
 # calculate savings as 10% of income (income * .1). (variable that is a float).
-percentSavings = income*.1
+savings = round(income*.1, 2)
 
 # calculate spending as income - savings - rent/mortgage - utilities - groceries - transportation (variable that is a float)
-spending = income-percentSavings-rent-utilities-groceries-transport
+spending = round(income-savings-rent-utilities-groceries-transport, 2)
 
 # caclulate percent income of rent/mortgage (rent / income * 100) (variable)
-percentRent = rent/income*100
+percentRent = round(rent/income*100, 1)
 
 # caclulate percent income of utilities (utilities / income * 100) (variable)
-percentUtilities = utilities/income*100
+percentUtilities = round(utilities/income*100, 1)
 
 # caclulate percent income of groceries (groceries / income * 100) (variable)
-percentGroceries = groceries/income*100
+percentGroceries = round(groceries/income*100, 1)
 
 # caclulate percent income of transportation (transportation / income * 100) (variable)
-percentTransport = transport/income*100
+percentTransport = round(transport/income*100 ,1)
+
+# calculate percent income of savings (percentsavings / income * 100) (variable)
+percentSavings = round(savings/income*100, 1)
 
 # calculte percent income of spending (spending / income * 100) (variable)
-percentSpending = spending/income*100
+percentSpending = round(spending/income*100, 1)
+
+# white space print statement
+print("")
 
 # Your rent is $XX.XX, which is XX% of your income (print statement).
-print("Your rent is", rent, ", which is", percentRent, "of your income.")
+print("Your rent is $", rent, ", which is", percentRent, "percent of your income.")
 
 # Your utilities is $XX.XX, which is XX% of your income (print statement).
+print("Your utilities cost $", utilities, ", which is", percentUtilities, "percent of your income.")
 
 # Your groceries is $XX.XX, which is XX% of your income (print statement).
+print("Your groceries cost $", groceries, ", which is", percentGroceries, "percent of your income.")
 
 # Your transportation is $XX.XX, which is XX% of your income (print statement).
+print("Your transportation costs $", transport, ", which is", percentTransport, "percent of your income.")
 
 # Your savings is $XX.XX, which is XX% of your income (print statement).
-
+print("Your savings are $", savings, ", which is", percentSavings, "percent of your income.")
 
 # Your spending is $XX.XX, which is XX% of your income (print statement).
+print("Your spending is $", spending, ", which is", percentSpending, "percent of your income. \n")
 
-
+# print statement that thanks the user and ends the conversation.
+print("Thank you for using the monthly financial calculator. Spend wisely!")
 
