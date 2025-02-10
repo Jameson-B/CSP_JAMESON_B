1e0df6a83e2e27cd0742b2f14e7acfb5d654ab84
Jameson-B <jameson.bultez@ucas-edu.net>
2025-02-10 11:49:08 -0700

    Created strings notes python.

---
+++ Strings/notes.py
@@ -0,0 +1,31 @@
+# Jameson Bultez, Strings Notes Python
+
+# String: a data type that holds information surrounded by quotiation marks "" ''.
+
+# Syntax: the conventions of a written language.
+
+# Stupid-proofing AKA sanitization: protecting against user craziness.
+
+# Bug: an error.
+
+# Syntax Error: spelling was incorrect.
+
+# Runtime Error: a user broke the program.
+
+# Type Error: data types did not agree.
+
+# Logic Error: programmer goofed, output will be incorrect.
+    # Print debugging.
+    # Start at the beginning, explaining it step by step.
+
+
+
+note = "Vienna's class"
+
+name = input("What is your first name? \n").strip().lower().capitalize()
+
+print(f"Hello, {name}, welcome to my program!")
+
+sentence = "The quick brown fox jumps over the lazy dog."
+
+german = "Wie geht es Ihnen?"
