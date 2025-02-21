751b744cbaa41520f62f33551b2cd195abb9d2e0
Jameson-B <jameson.bultez@ucas-edu.net>
2025-02-21 11:51:26 -0700

    Worked on financial calculator update Python.

--- Functions/financial_falculator_update.py
+++ Functions/financial_falculator_update.py
@@ -1,12 +1,24 @@
 # Jameson Bultez, Financial Calculator Update Python
 
-# Function that calculates percentage of income and prints the info.
-def info(cost, income, type):
-    percent = round(cost/income*100, 1)
-    print(f"Your {type} is ${cost:.2f}, which is {percent}% of your income. \n") 
+# Declare cost variables before they are defined.
+income = 0
+rent = 0
+utilities = 0
+groceries = 0
+transport = 0
 
-def inputs(var, category):
-    var = float(input(f"What is your monthly {category}?: \n"))
+
+# Set each cost variable to a user input and return the variable.
+def inputs(cost, category):
+    cost = input(f"What is your monthly {category}?: \n")
+    return cost
+
+# Function to caclulate percent income of different expenses and return string informing user.
+def advice(cost, income, category):
+    percent = round(cost/income*100, 1)
+    return f"Your {category} is ${round(cost, 2)}, which is {percent}% of your income.\n" 
+
+
 
 # print statement that welcomes my user and tells them what my program does.
 print("Welcome! This is the monthly financial calculator. \n")
@@ -15,7 +27,22 @@ inputs(income, "income")
 inputs(rent, "rent/mortgage")
 inputs(utilities, "utilities cost")
 inputs(groceries, "groceries cost")
-inputs(transport, "gas/bus fare cost")
+inputs(transport, "gas/bus fare")
+
+
+# calculate savings as 10% of income (income * .1). (variable that is a float).
+savings = round(income*.1, 2)
+
+# calculate spending as income - savings - rent/mortgage - utilities - groceries - transportation (variable that is a float)
+spending = round(income-savings-rent-utilities-groceries-transport, 2)
+
+print(advice(rent, income, "rent/mortage"))
+print(info(utilities, income, "utilities cost"))
+print(info(groceries, income, "groceries cost"))
+print(info(transport, income, "gas/bus fare"))
+print(f"You should be saving {savings}, which is 10% of your income.")
+print(f"You have {spending} left over to spend, which is 10% of your income.")
+
 
 '''
 # ask user what their income is (variable that is an input).
@@ -37,11 +64,7 @@ transport = float(input("How much does your gas/bus fare cost?: \n"))
 # white space print statement
 print("")
 
-# calculate savings as 10% of income (income * .1). (variable that is a float).
-savings = round(income*.1, 2)
 
-# calculate spending as income - savings - rent/mortgage - utilities - groceries - transportation (variable that is a float)
-spending = round(income-savings-rent-utilities-groceries-transport, 2)
 
 
 
