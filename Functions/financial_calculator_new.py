df0ac5ffc9c5d6a95e248948ab134827bd797335
Jameson-B <jameson.bultez@ucas-edu.net>
2025-02-26 06:19:52 +0000

    Updated finance calc. update python and c.

--- Functions/financial_calculator_new.py
+++ Functions/financial_calculator_new.py
@@ -3,15 +3,14 @@
 # Function to caclulate percent income of an expense and return string informing user.
 def info(cost, income, category):
     percent = round(cost/income*100, 1)
-    return f"Your monthly {category} is ${round(cost, 2)}, which is {percent}% of your income."
+    return f"{category}: {percent}% (${cost})"
 
 def user_inputs(expense):
     user_input = float(input(f"How much is your monthly {expense}?:\n"))
     return user_input
 
-
 # print statement that welcomes my user and tells them what my program does.
-print("\nWelcome! This is your monthly financial calculator.\nAll inputs should be in US Dollars. (Don't write the dollar $ sign.)\n")
+print("\nWelcome! This is your monthly financial calculator.\nIt will tell you what percent each part is of your income.\nAll inputs should be in US Dollars. (Don't write the dollar $ sign.)\n")
 
 income = user_inputs("income")
 rent = user_inputs("rent/mortage")
@@ -25,13 +24,12 @@ savings = round(income*.1, 2)
 # calculate spending as income - savings - rent/mortgage - utilities - groceries - transportation (variable that is a float)
 spending = round(income-savings-rent-utilities-groceries-transport, 2)
 
-print(info(rent, income, "rent/mortgage payment"))
-print(info(utilities, income, "utilities cost"))
-print(info(groceries, income, "groceries cost"))
-print(info(transport, income, "transportation cost"))
-
-print(f"You should be saving 10% of your income, which is ${savings}.")
-print(f"You you have ${spending} left over to spend.\n")
+print(info(rent, income, "Rent/mortage"))
+print(info(utilities, income, "Utilities"))
+print(info(groceries, income, "Groceries"))
+print(info(transport, income, "Transportation"))
+print(info(savings, income, "Savings"))
+print(info(spending, income, "Left over"))
 
 # print statement that thanks the user and ends the conversation.
 print("Thank you for using the monthly financial calculator. Spend wisely!\n")
