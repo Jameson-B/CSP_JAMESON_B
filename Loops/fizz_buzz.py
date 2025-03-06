b4098a9673415e2d0b490ce6e15d975e076a0e9d
Jameson-B <jameson.bultez@ucas-edu.net>
2025-03-06 11:49:45 -0700

    Started fizz buzz python.

---
+++ Loops/fizz_buzz.py
@@ -0,0 +1,7 @@
+# Jameson Bultez, Fizz Buzz Python
+
+# Print integers from 1 to 50 and replace multiples of 3 with "Fizz", mulitples of 5 with "Buzz", and multiplies of 3 and 5 with "FizzBuzz". 
+    # Set "x", the iterator, to 1 
+x = 1
+for x in range(1, 51, 1):
+    print(x)
