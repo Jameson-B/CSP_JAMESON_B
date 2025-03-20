8e9885f4b1ec423d8c04d9f50e2e98eacfaedc7a
Jameson-B <jameson.bultez@ucas-edu.net>
2025-03-20 11:49:14 -0600

    Co-authored-by: CasperHolt <CasperHolt@users.noreply.github.com> Co-authored-by: Robert-Nelson1 <Robert-Nelson1@users.noreply.github.com> Co-authored-by: davicito tu papacito <david.garciabermudez@ucas-edu.net>

---
+++ men.py
@@ -0,0 +1,49 @@
+# Men Python
+
+'''
+Assignment Instructions:
+
+Build a game! With your team (of 4 people) you need to build a game that will be run completely out of the console. Your game must include:
+
+1 variable
+2 Functions
+1 conditional
+1 loop
+
+'''
+
+
+# Robert N: Art.
+#print(________)
+#print(  |    |)
+#print(       |)
+#print(       |)
+#print(       |)
+#print(       |)
+
+#print(________)
+#print(  |    |)
+#print( "🙁   |")
+#print(       |)
+#print(       |)
+#print(       |)
+
+# Jameson B: Handle user inputs.
+    #1. Define variables and functions.
+        # Word list (str, list)
+        # Correct word (str)
+        # Correct word length (int)
+        # Letter guess (str)
+        # Word guess ()
+        
+    #2. Print user instructions.
+print("Welcome to Men!")
+guess = input("Guess one letter:\n")
+    #3. Computations.
+    #4. Output.
+    #5. Repeat.
+
+# David G: Compiling word list, random word selector.
+
+# Jasper Holt: outputs letter check, 
+
