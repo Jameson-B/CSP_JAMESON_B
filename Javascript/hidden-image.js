5fbee568bb5712a3367f9ba91039a0537d1838aa
Jameson-B <jameson.bultez@ucas-edu.net>
2025-05-06 12:13:39 -0600

    Finished hidden image.

--- Javascript/hidden-image.js
+++ Javascript/hidden-image.js
@@ -1,3 +1,7 @@
 function show() {
     document.getElementById("hidden-image").style.display = "block"
 }
+
+function self_destruct() {
+    document.getElementById("self-destruct-paragraph").style.display = "none"
+}
