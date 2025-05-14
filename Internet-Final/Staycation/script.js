eef4b27abe3a85a8cdc1119bb219b29550992d5e
Jameson-B <jameson.bultez@ucas-edu.net>
2025-05-14 11:51:01 -0600

    worked on hoverable image and show/hide button

--- Internet-Final/Staycation/script.js
+++ Internet-Final/Staycation/script.js
@@ -1,18 +1,31 @@
+let figure = document.getElementById("hoverable-figure")
+let img = document.querySelector("#hoverable-figure img");
+let caption = document.querySelector("#hoverable-figure figcaption")
+
 function swapFigureOnEnter() {
-    const img = document.querySelector(".card-container #hoverable-figure img");
-    const caption = document.querySelector(".card-container #hoverable-figure figcaption");
-
     img.src = "https://images.unsplash.com/photo-1662925429325-cd579a0a8ff1?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
-
-    caption.textContent = "A snowboarder mid-jump";
+    img.alt = "Snowboarder in midair at Brighton Ski Resort in Utah";
+    caption.innerHTML = '<p>A snowboarder mid-jump</p><p>Photo by <a href = "https://unsplash.com/@bariceric" target = "_blank" rel = "noopener">Eric Barrett</a> on <cite><a href = "https://unsplash.com/photos/a-person-jumping-in-the-air-on-a-snowboard-4RPWDdVdgAQ" target = "_blank" rel = "noopener">Unsplash</a></cite></p>';
 }
 
-/*
-<img class = "img-two" src = "https://images.unsplash.com/photo-1662925429325-cd579a0a8ff1?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                        alt = "Snowboarder in midair at Brighton Ski Resort in Utah"></img>
+function resetFigureOnLeave() {
+    img.src = "https://images.unsplash.com/photo-1662925440167-7d4a36a0f721?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    img.alt = "Snowboarder about to go off jump at Brighton Ski Resort in Utah";
+    caption.innerHTML = ' <p>A snowboarder prepares to make a jump in the terrain park</p><p>Photo by <a href = "https://unsplash.com/@bariceric" target = "_blank" rel = "noopener">Eric Barrett</a> on <cite><a href = "https://unsplash.com/photos/a-person-lying-on-the-snow-zpaQ3A6QsZU" target = "_blank" rel = "noopener">Unsplash</a></cite></p>';
+}
 
-<figcaption class = "caption-two">
-                            <p>A snowboarder mid-jump</p>
-                            <p>Photo by <a href = "https://unsplash.com/@bariceric" target = "_blank" rel = "noopener">Eric Barrett</a> on <cite><a href = "https://unsplash.com/photos/a-person-jumping-in-the-air-on-a-snowboard-4RPWDdVdgAQ" target = "_blank" rel = "noopener">Unsplash</a></cite></p>
-                        </figcaption>
-*/
+let button = document.getElementById("collapsible-button");
+let content = document.getElementById("collapsible-content");
+let buttonClicked = false;
+
+function showHideContent() {
+    if (buttonClicked == false){
+        buttonClicked = true;
+        button.textContent = "Hide";
+        content.style.display = flex;
+    } else {
+        buttonClicked = false;
+        buttonClicked = false;
+        button.textContent = "Show";
+    }
+}
